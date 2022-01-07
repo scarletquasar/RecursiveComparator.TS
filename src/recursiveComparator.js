6389959f24ad74cad7f1aa439cbc6b7fd0972b4b
EternalQuasar <70824102+EternalQuasar0206@users.noreply.github.com>
2022-01-07 14:03:56 -0300

    Update recursiveComparator.js

--- src/recursiveComparator.js
+++ src/recursiveComparator.js
@@ -1,10 +1,3 @@
-/*
-    recursiveComparator v1.2.0 - 12/2021
-    (C) 2021 Kayky Vitor Cruz
-    Variable comparison algorithm created to serve as an alternative to the "JSON.stringify" and "Array.every" 
-    methods, having higher operating speed and more reliable results. This code is licensed under MIT License.
-*/
-
 export function compare(value1, value2, cmpFn = (a, b) => a === b) {
     if(typeof value1 !== typeof value2) return cmpFn(value1, value2);
     switch(typeof value1) {
